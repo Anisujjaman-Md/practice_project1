7b8dd05736d235fefc19d3f4e87892acdc3b5171
Md-Anisujjaman <contact.anisujjaman@gmail.com>
2022-08-01 23:16:49 +0600

    Dynamic

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.frontpage, name='frontpage'),
+    path("<int:id>", views.frontpage, name='frontpage'),
+    path("", views.home, name = "home")
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
-def frontpage(response):
-    return HttpResponse("Hello, world. You're at the frontpage.")
+def frontpage(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return HttpResponse(f"<h1>{ls.name}</h1>")
+
+def home(response):
+    pass
